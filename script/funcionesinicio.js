94229ec429b03f90643ffd66f457f15a5566d76e
Candela Mancuello <delalaura98@mail.com>
2025-02-15 03:40:57 -0300

    paginas nuevas play list musical, algunos estilos

---
+++ script/funcionesinicio.js
@@ -0,0 +1,18 @@
+function calcularDias(fechaReferencia) {
+    const fechaRef = new Date(fechaReferencia); // Fecha publicacion
+    const hoy = new Date(); // Fecha actual
+
+    // Calcular la diferencia en días
+    const diferencia = Math.floor((hoy - fechaRef) / (1000 * 60 * 60 * 24));
+    return diferencia + "d";
+}
+
+// Fechas de publicacion
+const fechaBase1 = "2025-01-25"; 
+const fechaBase2 = "2025-02-03"; 
+const fechaBase3 = "2025-01-31"; 
+
+// Asignar valores dinámicamente
+document.getElementById("date1").textContent = calcularDias(fechaBase1);
+document.getElementById("date2").textContent = calcularDias(fechaBase2);
+document.getElementById("date3").textContent = calcularDias(fechaBase3);
